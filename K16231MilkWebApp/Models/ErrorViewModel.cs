16b20a80a23ccc7c6cad9182299df29c28865571
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2024-05-16 18:43:32 +0700

    Delete K16231MilkWebApp directory

--- K16231MilkWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace K16231MilkWebApp.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
