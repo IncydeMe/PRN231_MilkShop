7aa82eb13523b627b0bc7f340b9ca5c5c6f0f79d
stonk-over-flown <fyeah0515@gmail.com>
2024-05-17 17:23:05 +0700

    Added BlogCategory table and setup GetConnectionString

---
+++ MilkData/BlogCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MilkData;
+
+public partial class BlogCategory
+{
+    public int BlogCategoryId { get; set; }
+
+    public string BlogCategoryName { get; set; }
+
+    public virtual ICollection<Blog> Blogs { get; set; } = new List<Blog>();
+}
