befb97f5d0d1550d59c7a068dde722e370af3b6d
hntduong12345 <hntduong2003@gmail.com>
2024-05-17 17:35:28 +0700

    add authentication controller

---
+++ MilkBusiness/AccountBusiness.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MilkBusiness.Utils;
+using MilkData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkBusiness
+{
+    public class AccountBusiness
+    {
+        private readonly Net17112314MilkContext _context;
+
+        public AccountBusiness()
+        {
+            if( _context == null )
+                _context = new Net17112314MilkContext();
+        }
+
+        #region Authentization
+        public async Task<IMilkResult> Login(string email, string password)
+        {
+            Account account = await _context.Accounts.FirstOrDefaultAsync(a =>
+                                    a.Email.Equals(email) && a.Password.Equals(password));
+
+            MilkResult result = new MilkResult();
+
+            if( account == null )
+            {
+                result.Status = -1;
+                result.Message = "Incorrect Email or Password";
+                return result;
+            }
+
+            if (!account.IsActive)
+            {
+                result.Status = -1;
+                result.Message = "Your account is currently inactive (banned)";
+                return result;
+            }
+
+            result.Data = JwtUtil.GenerateJwtToken(account);
+            result.Status = 1;
+            result.Message = "Login successfully";
+            return result;
+        }
+
+        public async Task<IMilkResult> Register(string email, string password)
+        {
+            Account account = await _context.Accounts.FirstOrDefaultAsync(a =>
+                                    a.Email.Equals(email) && a.Password.Equals(password));
+
+            MilkResult result = new MilkResult();
+
+            if (account != null)
+            {
+                result.Status = -1;
+                result.Message = "Email has already used";
+                return result;
+            }
+
+            Account newAccount = new Account{
+                AccountId = 0,
+                FullName = email,
+                Password = password,
+                Email = email,
+                Phone = string.Empty,
+                Address = string.Empty,
+                Role = "Member",
+                IsActive = true
+            };
+            _context.Accounts.Add(newAccount);
+            await _context.SaveChangesAsync();
+
+            result.Data = JwtUtil.GenerateJwtToken(newAccount);
+            result.Status = 1;
+            result.Message = "Register successfully";
+            return result;
+        }
+        #endregion
+
+        #region Account
+        public async Task<IMilkResult> GetAllAccount()
+        {
+            var accList =  await _context.Accounts.ToListAsync();
+            return new MilkResult(accList);
+        }
+
+        public async Task<IMilkResult> GetAccountInfo(int accountId)
+        {
+            var acc = await _context.Accounts.FirstOrDefaultAsync(a => a.AccountId == accountId);
+            return new MilkResult(acc);
+        }
+        #endregion
+    }
+}
---
+++ MilkWebAPI/Constants/ApiEndPointConstant.cs
@@ -0,0 +1,15 @@
+﻿namespace MilkWebAPI.Constants
+{
+    public class ApiEndPointConstant
+    {
+        public const string RootEndPoint = "/api";
+        public const string ApiVersion = "/v1";
+        public const string ApiEndpoint = RootEndPoint + ApiVersion;
+
+        public class Authentication()
+        {
+            public const string LoginEndpoint = ApiEndpoint + "/login";
+            public const string RegisterEndpoint = ApiEndpoint + "/register";
+        }
+    }
+}
