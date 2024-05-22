7afcfc8db18cbfbcf3b8e10327be0be2284734be
NguyenTruongKhangThuan <ntkthuan1209@gmail.com>
2024-05-22 16:58:06 +0700

    Modify Staff UI

---
+++ MilkWebApp/Controllers/StaffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkWebApp.Controllers
+{
+	public class StaffController : Controller
+	{
+		public IActionResult Dashboard()
+		{
+			return View();
+		}
+	}
+}
