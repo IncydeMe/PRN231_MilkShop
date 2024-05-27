90d6e3f5171e903cb396a026c1916301ac413d20
hntduong12345 <hntduong2003@gmail.com>
2024-05-27 10:23:21 +0700

    fix gift business and voucher business

--- MilkBusiness/VoucherBusiness.cs
+++ MilkBusiness/VoucherBusiness.cs
@@ -1,108 +1,77 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using MilkData;
-//using MilkData.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using MilkData;
+using MilkData.Models;
+using MilkData.Repository.Implements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace MilkBusiness
-//{
-//	public class VoucherBusiness
-//	{
-//		private readonly VoucherDAO _voucherDAO;
+namespace MilkBusiness
+{
+	public class VoucherBusiness
+	{
+        private readonly UnitOfWork _unitOfWork;
 
-//		public VoucherBusiness()
-//		{
-//			_voucherDAO = new VoucherDAO();
-//		}
+        public VoucherBusiness()
+        {
+            _unitOfWork ??= new UnitOfWork();
+        }
 
-//		public async Task<IMilkResult> GetVoucherList()
-//		{
-//			var vouchers = await _voucherDAO.GetVoucherListAsync();
-//			try
-//			{
-//				if (vouchers == null)
-//				{
-//					return new MilkResult(-1, "Get voucher list failed", null);
-//				}
-//				else
-//				{
-//					return new MilkResult(1, "Get voucher list successfully", vouchers);
-//				}
-//			}
-//			catch (Exception ex)
-//			{
-//				return new MilkResult(-1, ex.Message, null);
-//			}
-//		}
+        public async Task<IMilkResult> GetVoucherList()
+        {
+            var vouchers = await _unitOfWork.GetRepository<Voucher>().GetListAsync();
 
-//		public async Task<IMilkResult> GetVoucherById(int id)
-//		{
-//			var voucher = await _voucherDAO.GetVoucherById(id);
-//			try
-//			{
-//				if (voucher == null)
-//				{
-//					return new MilkResult(-1, "Get voucher failed", null);
-//				}
-//				else
-//				{
-//					return new MilkResult(1, "Get voucher successfully", voucher);
-//				}
-//			}
-//			catch (Exception ex)
-//			{
-//				return new MilkResult(-1, ex.Message, null);
-//			}
-//		}
+            return new MilkResult(1, "Get voucher list successfully", vouchers);
+        }
 
-//		public async Task<IMilkResult> CreateVoucher(Voucher voucher)
-//		{
-//			try
-//			{
-//				await _voucherDAO.CreateVoucherAsync(voucher);
-//				return new MilkResult(1, "Create voucher successfully", voucher);
-//			}
-//			catch (Exception ex)
-//			{
-//				return new MilkResult(-1, ex.Message, null);
-//			}
-//		}
+        public async Task<IMilkResult> GetVoucherById(Guid id)
+        {
+            Voucher voucher = await _unitOfWork.GetRepository<Voucher>().SingleOrDefaultAsync(predicate: v => v.VoucherId.Equals(id));
 
-//		public async Task<IMilkResult> UpdateVoucher(Voucher voucher)
-//		{
-//			try
-//			{
-//				await _voucherDAO.UpdateVoucherAsync(voucher);
-//				return new MilkResult(1, "Update voucher successfully", voucher);
-//			}
-//			catch (Exception ex)
-//			{
-//				return new MilkResult(-1, ex.Message, null);
-//			}
-//		}
+            MilkResult result = new MilkResult(1, "Get voucher successfully", voucher);
+            return result;
+        }
 
-//		public async Task<IMilkResult> DeleteVoucher(int id)
-//		{
-//			var voucher = await _voucherDAO.GetVoucherById(id);
-//			try
-//			{
-//				if (voucher == null)
-//				{
-//					return new MilkResult(-1, "Delete voucher failed", null);
-//				}
-//				else
-//				{
-//					_voucherDAO.DeleteVoucher(voucher);
-//					return new MilkResult(1, "Delete voucher successfully", voucher);
-//				}
-//			}
-//			catch (Exception ex)
-//			{
-//				return new MilkResult(-1, ex.Message, null);
-//			}
-//		}
-//	}
-//}
+        public async Task<IMilkResult> CreateVoucher(Voucher voucher)
+        {
+            await _unitOfWork.GetRepository<Voucher>().InsertAsync(voucher);
+            var res = await _unitOfWork.CommitAsync();
+
+            if (res > 0)
+            {
+                return new MilkResult(1, "Create voucher successfully", voucher);
+            }
+            else
+            {
+                return new MilkResult();
+            }
+        }
+
+        public async Task<IMilkResult> UpdateVoucher(Voucher voucher)
+        {
+            _unitOfWork.GetRepository<Voucher>().UpdateAsync(voucher);
+            await _unitOfWork.CommitAsync();
+
+            return new MilkResult(1, "Update voucher successfully", voucher);
+        }
+
+        public async Task<IMilkResult> DeleteVoucher(Guid id)
+        {
+            Voucher voucher = await _unitOfWork.GetRepository<Voucher>().SingleOrDefaultAsync(predicate: v => v.VoucherId.Equals(id));
+
+            if (voucher == null)
+            {
+                return new MilkResult();
+            }
+            else
+            {
+                _unitOfWork.GetRepository<Voucher>().DeleteAsync(voucher);
+                await _unitOfWork.CommitAsync();
+
+                return new MilkResult(1, "Delete voucher successfully", voucher);
+            }
+        }
+    }
+}
