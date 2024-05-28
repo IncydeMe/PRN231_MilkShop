a8897f9bab135f53c0cafe939c6045546c24c782
NguyenTruongKhangThuan <ntkthuan1209@gmail.com>
2024-05-28 12:13:05 +0700

    Feedback List for Product
    Yes, I am finally using Bootstrap =))

--- MilkWebApp/Controllers/StaffController.cs
+++ MilkWebApp/Controllers/StaffController.cs
@@ -17,10 +17,22 @@ namespace MilkWebApp.Controllers
 			return View();
 		}
 
+		public IActionResult AddProduct()
+		{
+			return View();
+		}
+
 		public IActionResult ProductDetails()
 		{
             return View();
         }
+
+        #region Edit Product Options
+
+        public IActionResult EditProduct()
+		{
+			return View();
+		}
 
 		public IActionResult EditBasicProductDetails()
 		{
@@ -31,5 +43,62 @@ namespace MilkWebApp.Controllers
 		{
 			return View();
 		}
-	}
+
+        #endregion
+
+		public IActionResult ProductFeedbacks()
+		{
+			return View();
+		}
+
+		public IActionResult Order()
+		{
+			return View();
+		}
+
+		public IActionResult OrderDetails()
+		{
+			return View();
+		}
+
+		public IActionResult PreOrder()
+		{
+			return View();
+		}
+
+		public IActionResult PreOrderDetails()
+		{
+			return View();
+		}
+
+		public IActionResult Message()
+		{
+			return View();
+		}
+
+		public IActionResult MessageDetails()
+		{
+			return View();
+		}
+
+		public IActionResult Blogs()
+		{
+			return View();
+		}
+
+		public IActionResult BlogDetails()
+		{
+			return View(); 
+		}
+
+		public IActionResult AddBlog()
+		{
+			return View();
+		}
+
+		public IActionResult EditBlog()
+		{
+			return View();
+		}
+    }
 }
