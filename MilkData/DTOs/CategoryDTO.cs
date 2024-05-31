05f3432c7c45534bd237cfb7cd0f4d1e7edf3150
huynhnguyen-haku <huynhnguyen1532@gmail.com>
2024-06-01 02:21:25 +0700

    update api for creating, getting and deleting category

---
+++ MilkData/DTOs/CategoryDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkData.DTOs
+{
+    public class CategoryDTO
+    {
+        public class CreateCategoryDTO
+        {
+            public string CategoryName { get; set; } = null!;
+        }
+
+        public class UpdateCategoryDTO
+        {
+            public string CategoryName { get; set; } = null!;
+        }
+    }
+}
