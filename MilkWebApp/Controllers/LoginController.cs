5822e2e27fe472b1e230f3f5b1a321d9ac829295
TruongNLN <72696692+schoolsupertech@users.noreply.github.com>
2024-06-03 11:42:09 +0700

    Login Error

--- MilkWebApp/Controllers/LoginController.cs
+++ MilkWebApp/Controllers/LoginController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json;
 
 namespace MilkWebApp.Controllers;
 
 public class LoginController : Controller
 {
-    private readonly string apiUrl = "https://localhost:5140/Login";
+    private readonly string apiUrl = "https://localhost:7120/api/v1/";
 
     public ActionResult Index()
     {
@@ -17,8 +18,8 @@ public class LoginController : Controller
         return View();
     }
 
-    [HttpGet]
-    public Task<IActionResult> LoginRequest(string email, string password)
+    [HttpPost]
+    public async Task<IActionResult> LoginRequest(string email, string password)
     {
         if (string.IsNullOrEmpty(email))
         {
@@ -29,19 +30,22 @@ public class LoginController : Controller
             string err = "Empty password";
         }
 
+        Console.WriteLine("Email: " + email, "Password: " + password);
+
         return null;
 
         //try
         //{
-        //    var result = new List<Currency>();
+        //    string result = "";
         //    using (var httpClient = new HttpClient())
         //    {
-        //        using (var response = await httpClient.GetAsync(apiUrl + "GetAll"))
+        //        using (var response = await httpClient.GetAsync(apiUrl + "login?email=" + email + "&password=" + password))
         //        {
         //            if (response.IsSuccessStatusCode)
         //            {
         //                var content = await response.Content.ReadAsStringAsync();
-        //                result = JsonConvert.DeserializeObject<List<Currency>>(content);
+        //                result = JsonConvert.DeserializeObject<string>(content);
+        //                Console.WriteLine(result);
         //            }
         //        }
         //    }
