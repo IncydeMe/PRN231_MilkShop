2e8a63b27c9288387ef5a0647ebee0ebaa5d8244
stonk-over-flown <fyeah0515@gmail.com>
2024-07-04 16:46:08 +0700

    Added VNPay in OrderAPI

--- MilkBusiness/OrderBusiness.cs
+++ MilkBusiness/OrderBusiness.cs
@@ -2,22 +2,31 @@
 using MilkData.DTOs;
 using MilkData.Models;
 using MilkData.Repository.Implements;
+using Payment.Domain.VNPay.Config;
+using Payment.Domain.VNPay.Request;
+using Payment.Domain.VNPay.Response;
+using Payment.Service.Helper;
+using Payment.Service.VNPay;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static MilkData.DTOs.PaymentDTO;
 
 namespace MilkBusiness
 {
     public class OrderBusiness
     {
         private readonly UnitOfWork _unitOfWork;
+        private readonly IVNPayService _vNPayService;
 
         public OrderBusiness()
         {
             _unitOfWork ??= new UnitOfWork();
+            _vNPayService ??= new VNPayService();
         }
 
         #region Order
@@ -51,6 +60,7 @@ namespace MilkBusiness
             bool status = await _unitOfWork.CommitAsync() > 0;
             if (status)
             {
+                result.Data = CreatePayment(order);
                 result.Status = 1;
                 result.Message = "Order created successfully";
             } else
@@ -59,6 +69,159 @@ namespace MilkBusiness
                 result.Message = "Order creation failed";
             }
             return result;
+        }
+
+        public async Task ChangeOrderStatus(int orderId, string status)
+        {
+            var order = await _unitOfWork.GetRepository<Order>().SingleOrDefaultAsync(predicate: o => o.OrderId == orderId);
+            order.Status = status;
+            _unitOfWork.GetRepository<Order>().UpdateAsync(order);
+        }
+
+        #endregion
+
+        #region Payment
+
+        public async Task<Order> GetOrderObjectById(int id)
+        {
+            return await _unitOfWork.GetRepository<Order>().SingleOrDefaultAsync(predicate: o => o.OrderId == id);
+        }
+
+        private async Task<PaymentDTO.PaymentLinkResponse> CreatePayment(Order order)
+        {
+            VNPayConfig vnPayConfig = VNPayHelper.GetConfigData();
+
+            var orderInfo = await GetOrderObjectById(order.OrderId);
+
+            VNPayRequest request = new VNPayRequest()
+            {
+                vnp_Version = vnPayConfig.Version,
+                vnp_TmnCode = vnPayConfig.TmnCode,
+                vnp_CreateDate = DateTime.Now.ToString("yyyyMMddHHmmss"),
+                vnp_IpAddr = IPAddressHelper.GetLocalIPAddress(),
+                vnp_Amount = Math.Ceiling((decimal)orderInfo.TotalPrice) * 100,
+                vnp_CurrCode = vnPayConfig.CurrencyCode,
+                vnp_OrderType = "other",
+                vnp_OrderInfo = $"Ngày: {DateTime.Now.ToString("yyyyMMddHHmmss")}; Tổng giá: {orderInfo.TotalPrice}",
+                vnp_ReturnUrl = vnPayConfig.ReturnUrl,
+                vnp_TxnRef = order.OrderId.ToString(),
+                vnp_Command = "pay",
+                vnp_Locale = vnPayConfig.Locale
+            };
+
+            var paymentUrl = await _vNPayService.GetPaymentLink(vnPayConfig.PaymentUrl, vnPayConfig.HashSecret, request);
+
+            return new PaymentLinkResponse
+            {
+                OrderId = order.OrderId,
+                PaymentUrl = paymentUrl,
+            };
+        }
+
+        private async Task<PaymentOrderInfoResponse> TakeOrderInfo(int orderId)
+        {
+            decimal totalPrice = 0;
+            var orderDetails = await _unitOfWork.GetRepository<OrderDetail>().GetListAsync(predicate: od => od.OrderId == orderId);
+
+            if (orderDetails.Count > 0)
+            {
+                foreach (var orderDetail in orderDetails)
+                {
+                    var product = await _unitOfWork.GetRepository<Product>().SingleOrDefaultAsync(predicate: p => p.ProductId == orderDetail.ProductId);
+                    totalPrice += product.Price * orderDetail.Quantity;
+                }
+            }
+
+            return new PaymentOrderInfoResponse
+            {
+                ItemAmount = orderDetails.Count,
+                TotalPrice = totalPrice
+            };
+        }
+
+        public async Task<IMilkResult> CheckPaymentResponse(VNPayResponse response)
+        {
+            VNPayConfig vnPayConfig = VNPayHelper.GetConfigData();
+            PaymentReturnResponse paymentResponse = new PaymentReturnResponse();
+
+            paymentResponse.OrderId = int.Parse(response.vnp_TxnRef);
+            paymentResponse.Amount = response.vnp_Amount;
+
+            bool isValid = await _vNPayService.IsValidSignature(vnPayConfig.HashSecret, response);
+            if (isValid)
+            {
+                if (await GetOrderObjectById(int.Parse(response.vnp_TxnRef)) != null)
+                {
+                    if (response.vnp_ResponseCode == "00")
+                    {
+                        paymentResponse.PaymentStatus = "Success";
+                    }
+                    else
+                    {
+                        paymentResponse.PaymentStatus = "Unsuccess";
+                    }
+
+                    switch (response.vnp_ResponseCode)
+                    {
+                        case "00":
+                            paymentResponse.PaymentMessage = "Successful transaction.";
+                            break;
+                        case "07":
+                            paymentResponse.PaymentMessage = "Successful balance deduction. Suspicious transaction (Related to scam, abnormal transaction).";
+                            break;
+                        case "09":
+                            paymentResponse.PaymentMessage = "Card/Banking account is not registered banking services.";
+                            break;
+                        case "10":
+                            paymentResponse.PaymentMessage = "Incorrect Card/Banking Account infomation validation more than 3 times.";
+                            break;
+                        case "11":
+                            paymentResponse.PaymentMessage = "Transaction duration expired. Please redo making transaction.";
+                            break;
+                        case "12":
+                            paymentResponse.PaymentMessage = "Card/Banking Account is currently unavailable (Locked).";
+                            break;
+                        case "13":
+                            paymentResponse.PaymentMessage = "Wrong OTP Code inputed.";
+                            break;
+                        case "24":
+                            paymentResponse.PaymentMessage = "Transaction Canceled.";
+                            break;
+                        case "51":
+                            paymentResponse.PaymentMessage = "Banking Account's balance is not enough for this transaction.";
+                            break;
+                        case "65":
+                            paymentResponse.PaymentMessage = "Bankiing Account exceeds the transaction limitation per day.";
+                            break;
+                        case "75":
+                            paymentResponse.PaymentMessage = "Bank in maintanance.";
+                            break;
+                        case "79":
+                            paymentResponse.PaymentMessage = "Incorrect transaction's password inputed more than specified number of times.";
+                            break;
+                        case "99":
+                            paymentResponse.PaymentMessage = "Other Transaction Error.";
+                            break;
+                    }
+                }
+                else
+                {
+                    paymentResponse.PaymentStatus = "Unsuccess";
+                    paymentResponse.PaymentMessage = "Can't find order in DB.";
+                }
+
+            }
+            else
+            {
+                paymentResponse.PaymentStatus = "Unsuccess";
+                paymentResponse.PaymentMessage = "Invalid signature in response.";
+            }
+
+            return new MilkResult
+            {
+                Status = 1,
+                Data = paymentResponse
+            };
         }
 
         #endregion
--- MilkWebAPI/Controllers/OrderController.cs
+++ MilkWebAPI/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using MilkBusiness;
 using MilkData.DTOs;
 using MilkData.Models;
 using MilkWebAPI.Constants;
+using Payment.Domain.VNPay.Response;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace MilkWebAPI.Controllers
@@ -48,6 +49,42 @@ namespace MilkWebAPI.Controllers
                 return Ok(response);
             else
                 return BadRequest(response);
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Payment Response")]
+        public async Task<IActionResult> PaymentResponse([FromQuery] VNPayResponse response)
+        {
+            try
+            {
+                var result = await _orderBusiness.CheckPaymentResponse(response);
+                PaymentDTO.PaymentReturnResponse paymentResponse = (PaymentDTO.PaymentReturnResponse)result.Data;
+                var order = await _orderBusiness.GetOrderById(paymentResponse.OrderId);
+                int accountId = ((Order)order.Data).AccountId;
+
+                if (paymentResponse.PaymentStatus.Equals("Success"))
+                {
+                    await _orderBusiness.ChangeOrderStatus(paymentResponse.OrderId, "Paid");
+                }
+
+                PaymentDTO.PaymentReturnResponse paymentReturnResponse = new PaymentDTO.PaymentReturnResponse
+                {
+                    OrderId = paymentResponse.OrderId,
+                    PaymentStatus = paymentResponse.PaymentStatus,
+                    PaymentMessage = paymentResponse.PaymentMessage,
+                    Amount = paymentResponse.Amount
+                };
+                //redirect or ok?
+                return Ok(paymentReturnResponse);
+            }
+            catch (Exception ex)
+            {
+                if (ex.GetType() == typeof(BadHttpRequestException))
+                {
+                    return BadRequest(ex.Message);
+                }
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
