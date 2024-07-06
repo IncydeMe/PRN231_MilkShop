f6b1634459cb72071b7626f77e5e00077d26f7a6
stonk-over-flown <fyeah0515@gmail.com>
2024-07-07 00:16:39 +0700

    Added OrderAPI, OrderDetailAPI, and VNPay

---
+++ MilkData/DTOs/Order/OrderDTOs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkData.DTOs.Order
+{
+    public class OrderDTO
+    {
+        public int OrderId { get; set; }
+        
+        public int VoucherId { get; set; }
+
+        public Guid AccountId { get; set; }
+
+        public decimal OrderPrice { get; set; }
+
+        public string Status { get; set; } = null!;
+
+
+        //public class CreateOrderDTO
+        //{
+        //    //Order
+        //    public int AccountId { get; set; }
+
+        //    public Guid VoucherCode { get; set; }
+
+        //    public float TotalPrice { get; set; }
+
+        //    public string Status { get; set; } = null!;
+
+
+        //    //OrderDetail
+        //    public List<CreateOrderDetailDTO> OrderDetails { get; set; }
+        //}
+
+        //public class CreateOrderDetailDTO
+        //{
+
+        //    public int Quantity { get; set; }
+
+        //    public int ProductId { get; set; }
+        //}
+    }
+}
