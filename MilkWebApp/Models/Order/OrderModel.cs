d9d78d80738259a52a760ee385ac91c33b8918a8
stonk-over-flown <fyeah0515@gmail.com>
2024-07-17 14:13:24 +0700

    Patch Order Functions

---
+++ MilkWebApp/Models/Order/OrderModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MilkWebApp.Models.Order;
+
+public partial class OrderModel
+{
+    public int OrderId { get; set; }
+
+    public int AccountId { get; set; }
+
+    public string? Description { get; set; }
+
+    public Guid VoucherCode { get; set; }
+
+    public float TotalPrice { get; set; }
+
+    public string Currency { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public string PaymentType { get; set; } = null!;
+
+    public DateTime CreateDate { get; set; }
+
+    public DateTime UpdateDate { get; set; }
+
+    public string? Note { get; set; }
+
+    public string HttpMethod { get; set; }
+}
